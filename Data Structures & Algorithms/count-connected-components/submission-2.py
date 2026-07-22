5d7fdf254b434c6ca99b4c162ca27214358b8d0d
Tanishk Maheshwari <PES1UG23AM334@pesu.pes.edu>
2026-07-22 12:06:31 +0530

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        preMap = {i:[] for i in range(n)}
+        visit = set()
+        for csr,pre in edges:
+            preMap[csr].append(pre)
+            preMap[pre].append(csr)
+
+        def dfs(i):
+            if i in visit:
+                return 
+            visit.add(i)
+            for nei in preMap[i]:
+                dfs(nei)
+
+        count = 0 
+        for i in range(n):
+            if i not in visit:
+                dfs(i)
+                count += 1
+        return count
+        
+       
+        
+        
+
+
+
+        
